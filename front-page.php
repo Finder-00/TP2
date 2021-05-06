e5a7695972d48444c457ca7a1ec7a9643c4ecbb2
Finder-00 <arthurrobitaille@protonmail.com>
2021-05-06 12:18:38 -0400

    modif ajout nouvelle

--- front-page.php
+++ front-page.php
@@ -74,15 +74,20 @@ get_header();
 				<h3>Ajouter une nouvelle</h3>
 				<input type="text" name="title" placeholder="titre">
 				<textarea name="content"></textarea>
-				<button id="bout-rapide">Ajouter la nouvelle</button>
+				<?php endif;?>
+				<!-- Affiche les 3 derniere nouvelles et publie une nouvelle -->
+				<div class="conteneurBoutNouvelle">
+
+					<section class="derniereNouvelles">	
+						<button id="bout_nouvelles"> Afficher les 3 dernières nouvelles</button>
+						<section></section>
+					</section>
+
+					<?php if(current_user_can( 'administrator')) : ?>
+						<button id="bout-rapide">Ajouter la nouvelle</button>
+					<?php endif;?>
+				</div>
 				<?php ?>
-			</section>
-			<?php endif;?>
-			
-			<!-- Affiche les 3 derniere nouvelles -->
-			<section class="nouvelles">	
-				<button id="bout_nouvelles"> Afficher les 3 dernières nouvelles</button>
-				<section></section>
 			</section>
 
 	</main><!-- #main -->
@@ -136,6 +141,6 @@ function type_vague($typeCours){
 			return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(255, 169, 112)" fill-opacity="1" d="M0,320L34.3,304C68.6,288,137,256,206,202.7C274.3,149,343,75,411,80C480,85,549,171,617,213.3C685.7,256,754,256,823,250.7C891.4,245,960,235,1029,234.7C1097.1,235,1166,245,1234,256C1302.9,267,1371,277,1406,282.7L1440,288L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>';
 
 		case "Conception" :
-			return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(255, 134, 78)" fill-opacity="0.7" d="M0,128L11.4,133.3C22.9,139,46,149,69,165.3C91.4,181,114,203,137,186.7C160,171,183,117,206,128C228.6,139,251,213,274,213.3C297.1,213,320,139,343,101.3C365.7,64,389,64,411,53.3C434.3,43,457,21,480,42.7C502.9,64,526,128,549,138.7C571.4,149,594,107,617,80C640,53,663,43,686,69.3C708.6,96,731,160,754,170.7C777.1,181,800,139,823,154.7C845.7,171,869,245,891,277.3C914.3,309,937,299,960,298.7C982.9,299,1006,309,1029,304C1051.4,299,1074,277,1097,224C1120,171,1143,85,1166,69.3C1188.6,53,1211,107,1234,144C1257.1,181,1280,203,1303,208C1325.7,213,1349,203,1371,170.7C1394.3,139,1417,85,1429,58.7L1440,32L1440,320L1428.6,320C1417.1,320,1394,320,1371,320C1348.6,320,1326,320,1303,320C1280,320,1257,320,1234,320C1211.4,320,1189,320,1166,320C1142.9,320,1120,320,1097,320C1074.3,320,1051,320,1029,320C1005.7,320,983,320,960,320C937.1,320,914,320,891,320C868.6,320,846,320,823,320C800,320,777,320,754,320C731.4,320,709,320,686,320C662.9,320,640,320,617,320C594.3,320,571,320,549,320C525.7,320,503,320,480,320C457.1,320,434,320,411,320C388.6,320,366,320,343,320C320,320,297,320,274,320C251.4,320,229,320,206,320C182.9,320,160,320,137,320C114.3,320,91,320,69,320C45.7,320,23,320,11,320L0,320Z"></path></svg>';
+			return '<!--?xml version="1.0" standalone="no"?-->              <svg id="sw-js-blob-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">                    <defs>                         <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">                            <stop id="stop1" stop-color="rgba(255, 118.408, 62.706, 0.67)" offset="0%"></stop>                            <stop id="stop2" stop-color="rgba(251, 31, 72.366, 0.66)" offset="100%"></stop>                        </linearGradient>                    </defs>                <path fill="url(#sw-gradient)" d="M24.3,-25C31.5,-17,37.5,-8.5,38.7,1.2C39.9,10.9,36.2,21.7,29,29.8C21.7,37.9,10.9,43.3,1,42.3C-8.9,41.3,-17.9,34.1,-25.3,26C-32.8,17.9,-38.8,8.9,-39.4,-0.5C-39.9,-10,-34.9,-20,-27.4,-27.9C-20,-35.9,-10,-41.8,-0.7,-41.1C8.5,-40.3,17,-32.9,24.3,-25Z" width="100%" height="100%" transform="translate(50 50)" style="transition: all 0.3s ease 0s;" stroke-width="0" stroke="url(#sw-gradient)"></path>              </svg>';
 	}
 }
